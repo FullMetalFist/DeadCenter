322fe6ef96de84b81136b2b02ebd22311801a59f
Michael Vilabrera <m.vilabrera@gmail.com>
2017-12-01 12:19:02 -0500

    initial commit

---
+++ DeadCenter/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  DeadCenter
+//
+//  Created by Michael Vilabrera on 12/1/17.
+//  Copyright © 2017 Michael Vilabrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hey There", message: "This is nice", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Sweet", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
