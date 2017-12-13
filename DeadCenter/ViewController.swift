a87cd6b135a85894fc6a23ef3ff308e46ad9e7d8
Michael Vilabrera <m.vilabrera@gmail.com>
2017-12-12 19:41:16 -0500

    complete chapter 4

--- DeadCenter/ViewController.swift
+++ DeadCenter/ViewController.swift
@@ -11,10 +11,20 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 50
+    
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        currentValue = lroundf(slider.value)
+//
+//        targetValue = 1 + Int(arc4random_uniform(100))
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,17 +37,29 @@ class ViewController: UIViewController {
 //        let action = UIAlertAction(title: "Sweet", style: .default, handler: nil)
 //        alert.addAction(action)
 //        present(alert, animated: true, completion: nil)
-        let message = "current value is: \(currentValue)"
+        let message = "current value is: \(currentValue)" + "\nthe target value is: \(targetValue)"
         let alert = UIAlertController(title: "Heylo World", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         print("slider value is now: \(slider.value)")
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+//        currentValue = 50       // exercise: modify code so slider does not reset
+//        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 }
 
